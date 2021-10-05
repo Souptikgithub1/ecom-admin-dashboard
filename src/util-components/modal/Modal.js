70e95cf1e081f27e10b11ad5c712f969d0dfc289
Souptikgithub1 <souptik.karmakar93@gmail.com>
2021-10-05 21:57:46 +0530

    slide up animation for modal

--- src/util-components/modal/Modal.js
+++ src/util-components/modal/Modal.js
@@ -1,11 +1,18 @@
 import './Modal.css'
 import React from "react";
-import {Dialog, DialogContent, DialogTitle} from "@material-ui/core";
+import {Dialog, DialogContent, DialogTitle, Slide} from "@material-ui/core";
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
 
 const Modal = ({open, setOpen, header, children, footer, style={}}) => {
 
-    return <Dialog open={open} onClose={setOpen}>
+    return <Dialog
+        open={open}
+        onClose={() => setOpen(false)}
+        TransitionComponent={Transition}
+    >
         <DialogTitle style={{textTransform: "uppercase", display: "flex", justifyContent:"center"}}>{header}</DialogTitle>
         <DialogContent>
             {children}
